bffbe64442492ecde11498d2c09cec3e87bdf6ab
laucacot <cacot.laura@gmail.com>
2017-10-04 11:19:33 -0400

    automatisation de la premiere partie soit dndt pour les trois espece H SiH4 SiH3

--- rosenbrock4.cpp
+++ rosenbrock4.cpp
@@ -46,19 +46,77 @@ const value_type n_e = 1.e16;
 const int Nbr_espece=3;
 const int Te=3;
 
+const int Nbr_K=1;
+int jmax=Nbr_K;
+int imax=9;
+double **Tab;
 
+value_type p1,p2,g1,g2,g3,g4,Tp,Tx,Tj;
+state_type Kt(jmax, 0.0);
 const float   k6=1.83E-9*pow((Te),-1)*exp(-(10.68)/(Te));
 
 
 struct nsystem
 {
     void operator()( const state_type &n , state_type &dndt , const value_type &t ) 
-    { /*0=SiH3  1=SiH4  2=H*/
+    { /*0=SiH3  1=SiH4  2=H
         dndt( 0 ) = k6*n_e*n(1);
         dndt( 1 ) =-k6*n_e*n(1) ;
-	dndt( 2 ) = k6*n_e*n(1);
-    }
+	dndt( 2 ) = k6*n_e*n(1);*/
+    
+for (int k=0;k<3;k++)
+{
+dndt[k]=0;
+}
+value_type p1,p2,g1,g2,g3,g4,Tp,Tx;
+state_type Kt(jmax, 0.0);
+int i=0;
+for (int j=0;j<jmax;j++)
+{
 
+ p1=Tab[0][j];
+ p2=Tab[1][j];
+ g1=Tab[2][j];
+ g2=Tab[3][j];
+ g3=Tab[4][j];
+ g4=Tab[5][j]; 
+ Tp=(p2==0 or g3==0)?Te:Tg;
+
+ Kt[j]={Tab[6][j]*pow(Tp,Tab[7][j])*exp(-Tab[8][j]/Tp)};
+/*if (j==0)
+{
+cerr<<Kt[j]<<endl;
+
+} verification k ok !*/
+
+if (p1==200)
+{
+Tx=n_Ar*n[p2]*Kt[j];
+}
+else if (p2==100)
+{
+Tx=n[p1]*Kt[j];
+}
+else if (p2==10)
+{
+Tx=n[p1]*n_e*Kt[j];
+}
+else
+{
+Tx=n[p1]*n[p2]*Kt[j];
+}
+
+
+if(p1!=200) {dndt[p1]=dndt[p1]-Tx;}
+if(p2!=100) {dndt[p2]=dndt[p2]-Tx;}
+if(g1!=200) {dndt[g1]=dndt[g1]+Tx;}
+if(g2!=100) {dndt[g2]=dndt[g2]+Tx;}
+if(g3!=100) {dndt[g3]=dndt[g3]+Tx;}
+if(g4!=100) {dndt[g4]=dndt[g4]+Tx;}
+
+
+}
+}
 };
 
 struct jacobian
@@ -93,6 +151,36 @@ void write_density( const value_type t, const value_type Te, const state_type &n
 
 int main(int argc, char **argv)
 {
+ifstream fichier_k ("/home/cacot/Documents/rosenbrock/auto.dat");
+
+Tab = new double*[imax];
+Tab[0] = new double[imax*jmax];
+
+	for(int i=1;i<imax;i++)
+	{
+	Tab[i]=Tab[i-1]+jmax;
+	}
+
+if(fichier_k)
+{
+    //Tout est prêt pour la lecture.
+cerr<<"fichier ouvert"<<endl;
+
+
+for(int j=0;j<jmax;j++)
+{
+
+           	fichier_k>>Tab[0][j]>>Tab[1][j]>>Tab[2][j]>>Tab[3][j]>>Tab[4][j]
+		>>Tab[5][j]>>Tab[6][j]>>Tab[7][j]>>Tab[8][j];
+       		
+}
+fichier_k.close();
+
+}
+else
+{
+    cerr << "ERREUR: Impossible d'ouvrir le fichier en lecture." << endl;
+}
 cout <<"t"<<'\t'<<"Te"<<'\t'<<"SiH3"<<'\t'<<"SiH4"<<'\t'<<"H"<<endl;
 
 // Time variables
@@ -109,7 +197,7 @@ cout <<"t"<<'\t'<<"Te"<<'\t'<<"SiH3"<<'\t'<<"SiH4"<<'\t'<<"H"<<endl;
   state_type n_ini(Nbr_espece, 0.0); // initial conditions
   n_ini[0] = 0;
   n_ini[1] = n_SiH4_ini;
-n_ini[2]=0;
+  n_ini[2]=0;
  
   state_type n_new(Nbr_espece, 0.0);  // first step same as initial conditions
   n_new = n_ini;
